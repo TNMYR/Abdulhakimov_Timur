6e0c1c182c9a9d614b309c95309e60b8aba9e5ba
TNMYR <110169428+TNMYR@users.noreply.github.com>
2025-09-23 00:05:33 +0500

    Lone digger

--- Абдулхакимов_Тимур_ex.py
+++ Абдулхакимов_Тимур_ex.py
@@ -2,6 +2,7 @@ def main():
     print("Абдулхакимов Тимур")
     print("ИВТ-ИСУ-202Б, 2 подгруппа")
     print("22 сентября 2025")
+    print("Hey, brother, what you thinkin'? Leave that old record spinning")
 
 if __name__ == "__main__":
     main()
